85d1bec08d8c5b04fa958e39354e6f8e0323ed8c
DimitriKENGNE <m.dimitri.kengne@gmail.com>
2018-06-20 11:37:25 +0100

    Ajout des liens utiles dans la barre de navigation

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,12 +1,14 @@
 <div class="container">
     <nav class="nav blog-nav">
-        <a class="nav-link active" href="#">Home</a>
-        <a class="nav-link" href="#">New features</a>
-        <a class="nav-link" href="#">Press</a>
-        <a class="nav-link" href="#">New hires</a>
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link active" href="/">Home</a>
+        <a class="nav-link" href="/register">Register</a>
+        @if (!Auth::check())
+        <a class="nav-link" href="/login">Login</a>
+        @endif
         @if (Auth::check())
+            <a class="nav-link" href="/posts/create">Create a post</a>
             <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+            <a class="nav-link ml-auto" href="/logout">Logout</a>
         @endif
     </nav>
 </div>
